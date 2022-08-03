6cf992de96af9cfd7209adf35aaf15e272773f52
DHC <admin@dhcne.org>
2022-08-02 23:00:36 -0400

    updated location of tinylog.properties

--- dhc/izpack/installer/dhc.sh
+++ dhc/izpack/installer/dhc.sh
@@ -1,1 +1,1 @@
-java -Xmx256m -cp dhc.jar:plugins/* org.dhc.Main -d "data" -p "5000" -k "config/key.csv"
+java -Xmx128m -Dtinylog.configuration=config/tinylog.properties -cp dhc.jar:plugins/* org.dhc.Main -d "data" -p "5000" -k "config/key.csv"
--- dhc/izpack/installer/dhcgui.bat
+++ dhc/izpack/installer/dhcgui.bat
@@ -1,1 +1,1 @@
-java -Xmx256m -cp dhc.jar;plugins/* org.dhc.gui.Main -d "data" -p "5000" -k "config/key.csv"
+java -Xmx256m -Dtinylog.configuration=config/tinylog.properties -cp dhc.jar;plugins/* org.dhc.gui.Main -d "data" -p "5000" -k "config/key.csv"
